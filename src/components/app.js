c35efa9d182686e7cab13a522e1e614e93ff9090
Justin Tuttle <reddhouse@gmail.com>
2016-04-17 17:42:43 -0700

    Add index redirect and combine multiple components on welcome route

--- src/components/app.js
+++ src/components/app.js
@@ -1,3 +1,4 @@
+import styles from './app.css';
 import React from 'react';
 import { Component } from 'react';
 
@@ -6,13 +7,21 @@ export default class App extends Component {
     return (
       <div>
         <div className="Header">
-          <div className ="mainLogo">Pyrex Party!</div>
-          <div className="subLogo">Turn one home-cooked meal into many.</div>
+          <div className ={ styles.mainLogo }>Pyrex Party!</div>
+          <div className={ styles.subLogo }>Turn one home-cooked meal into many.</div>
         </div>
-      	{/* Children will arrive here from routes.js, automatically via 
-      		react-router so long as the routes are nested under App */}
-      	{this.props.children}
-        <div className="Footer">Copyright © 2016 PyrexParty, LLC</div>
+        <div className={ styles.currentComponent }>
+          <div className={ styles.firstThird }></div>
+          <div className={ styles.secondThird }>
+      	   {/* Children will arrive here from routes.js, automatically via 
+      		  react-router so long as the routes are nested under App */}
+      	   {this.props.children}
+           </div>
+           <div className={ styles.thirdThird }></div>
+        </div>
+        <div className={ styles.footer }>
+          Copyright © 2016 PyrexParty, LLC
+        </div>
       </div>
     );
   }
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Route, IndexRoute } from 'react-router';
+import { Route, IndexRedirect } from 'react-router';
 
 import App from './components/app';
+import Welcome from './containers/welcome';
 import GroupCardList from './containers/group-card-list';
-import Instructions from './components/instructions';
 
 // Establish mapping between various routes and components
 export default (
 <Route path="/" component={App}>
-	<IndexRoute component={Instructions} />
-	<Route path="groups" component={GroupCardList} />
+	<IndexRedirect to="/welcome" />
+	<Route path="welcome" components={Welcome}/>
+	<Route path="groups" component={GroupCardList}/>
 </Route>
 );
